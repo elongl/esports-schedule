05f6bfea1ea5b3546b6c4aa87f706cf50ef5b1b9
Elon Gliksberg <elongliks@gmail.com>
2024-07-19 22:05:18 +0300

    Added game support for server.

--- main_server.py
+++ main_server.py
@@ -3,6 +3,7 @@ from functools import lru_cache
 
 from fastapi import FastAPI, Response
 
+from game import Game
 from tournaments_api import TournamentsApi
 from tournaments_calendar import TournamentsCalendar
 
@@ -14,16 +15,21 @@ def get_ttl_hash(seconds=3600):
     return round(time.time() / seconds)
 
 
-@lru_cache(maxsize=1)
-def get_calendar(ttl_hash=None):
-    tournaments = TournamentsApi().get()
+@lru_cache
+def get_calendar(game: Game, ttl_hash=None):
+    tournaments = TournamentsApi(game).get()
     calendar = TournamentsCalendar(tournaments)
     return calendar.get_ical()
 
 
-@app.get("/")
-async def root():
+@app.get("/calendar/{game}")
+async def calendar(game: Game):
     return Response(
-        content=get_calendar(ttl_hash=get_ttl_hash()),
+        content=get_calendar(game, ttl_hash=get_ttl_hash()),
         media_type="text/calendar",
     )
+
+
+@app.get("/")
+async def root():
+    return "Use /calendar/{game} instead."
