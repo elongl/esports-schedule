cdc6bd59cfca94682cdcfc9dd955f3846363e40a
Elon Gliksberg <elongliks@gmail.com>
2024-07-19 21:59:16 +0300

    HTML parsing matches each game.

--- tournaments_api.py
+++ tournaments_api.py
@@ -70,6 +70,36 @@ class Tournament(BaseModel):
         raise ValueError(f"Invalid date format: {date}")
 
 
+class TournamentDivClassSelectors(BaseModel):
+    table: list[str]
+    row: list[str]
+    row_title: list[str]
+    row_date: list[str]
+    row_prize: list[str]
+    row_team_count: list[str]
+    row_location: list[str]
+
+
+_DIV_CLASS_IDS_PRESET1 = TournamentDivClassSelectors(
+    table=["gridTable", "tournamentCard"],
+    row=["gridRow"],
+    row_title=["gridCell", "Tournament", "Header"],
+    row_date=["gridCell", "EventDetails", "Date", "Header"],
+    row_prize=["gridCell", "EventDetails", "Prize", "Header"],
+    row_team_count=["gridCell", "EventDetails", "PlayerNumber", "Header"],
+    row_location=["gridCell", "EventDetails", "Location", "Header"],
+)
+_DIV_CLASS_IDS_PRESET2 = TournamentDivClassSelectors(
+    table=["divTable"],
+    row=["divRow"],
+    row_title=["divCell", "Tournament"],
+    row_date=["divCell", "EventDetails-Left-55"],
+    row_prize=["divCell", "EventDetails-Right-45"],
+    row_team_count=["divCell", "EventDetails-Right-40"],
+    row_location=["divCell", "EventDetails-Left-60"],
+)
+
+
 class TournamentsApi:
     _DATASOURCE_URL = "https://liquipedia.net"
     _TOURNAMENTS_URL_MAP = {
@@ -79,6 +109,15 @@ class TournamentsApi:
         Game.VALORANT: "/valorant/S-Tier_Tournaments",
         Game.DOTA2: "/dota2/Tier_1_Tournaments",
         Game.APEX_LEGENDS: "/apexlegends/S-Tier_Tournaments",
+    }
+
+    _GAME_DIV_CLASS_MAP = {
+        Game.COUNTER_STRIKE: _DIV_CLASS_IDS_PRESET1,
+        Game.VALORANT: _DIV_CLASS_IDS_PRESET1,
+        Game.DOTA2: _DIV_CLASS_IDS_PRESET1,
+        Game.ROCKET_LEAGUE: _DIV_CLASS_IDS_PRESET2,
+        Game.LEAGUE_OF_LEGENDS: _DIV_CLASS_IDS_PRESET2,
+        Game.APEX_LEGENDS: _DIV_CLASS_IDS_PRESET2,
     }
 
     def __init__(self, game: Game) -> None:
@@ -97,33 +136,42 @@ class TournamentsApi:
 
     def _parse_html(self, html: BeautifulSoup) -> list[Tournament]:
         tournaments = []
-        tournament_tables = html.find_all(
-            "div", class_=["tournamentCard", "tournament-card"]
+        tournament_tables = self._find_divs_with_classes(
+            html,
+            self._GAME_DIV_CLASS_MAP[self.game].table,
         )
         if not tournament_tables:
             raise ValueError("No tournaments found.")
 
         for tournament_table in tournament_tables:
-            tournament_rows = tournament_table.find_all(
-                "div", class_=["gridRow", "divRow"]
+            tournament_rows = self._find_divs_with_classes(
+                tournament_table,
+                self._GAME_DIV_CLASS_MAP[self.game].row,
             )
-            if not tournament_rows:
-                raise ValueError("No tournament rows found.")
-
             for tournament_row in tournament_rows:
                 tournaments.append(self._parse_row(tournament_row))
 
+        if not tournaments:
+            raise ValueError("No tournaments found.")
         return tournaments
 
     def _parse_row(self, tournament_row: BeautifulSoup) -> Tournament:
         title = self._locate_title(tournament_row)
-        date = tournament_row.find("div", class_="gridCell EventDetails Date Header")
-        prize = tournament_row.find("div", class_="gridCell EventDetails Prize Header")
-        team_count = tournament_row.find(
-            "div", class_="gridCell EventDetails PlayerNumber Header"
+        date = self._find_div_with_classes(
+            tournament_row,
+            self._GAME_DIV_CLASS_MAP[self.game].row_date,
         )
-        location = tournament_row.find(
-            "div", class_="gridCell EventDetails Location Header"
+        prize = self._find_div_with_classes(
+            tournament_row,
+            self._GAME_DIV_CLASS_MAP[self.game].row_prize,
+        )
+        team_count = self._find_div_with_classes(
+            tournament_row,
+            self._GAME_DIV_CLASS_MAP[self.game].row_team_count,
+        )
+        location = self._find_div_with_classes(
+            tournament_row,
+            self._GAME_DIV_CLASS_MAP[self.game].row_location,
         )
         url = urljoin(self._DATASOURCE_URL, title["href"])
 
@@ -136,13 +184,38 @@ class TournamentsApi:
             url=url,
         )
 
-    @staticmethod
-    def _locate_title(tournament_row: BeautifulSoup) -> BeautifulSoup:
-        potential_titles = tournament_row.find(
-            "div", class_="gridCell Tournament Header"
-        ).find_all("a")
-
+    def _locate_title(self, tournament_row: BeautifulSoup) -> BeautifulSoup:
+        title = self._find_div_with_classes(
+            tournament_row,
+            self._GAME_DIV_CLASS_MAP[self.game].row_title,
+        )
+        potential_titles = title.find_all("a")
         for potential_title in potential_titles:
             if potential_title.parent.name == "span":
                 continue
             return potential_title
+
+    def _find_divs_with_classes(
+        self,
+        element: BeautifulSoup,
+        classes: list[str],
+    ) -> list[BeautifulSoup]:
+        """
+        Finds div elements with *all* the given classes.
+        """
+        matching_divs = []
+        divs_to_check = element.find_all("div", class_=classes)
+        for potential_div in divs_to_check:
+            if all(cls in potential_div["class"] for cls in classes):
+                matching_divs.append(potential_div)
+        return matching_divs
+
+    def _find_div_with_classes(
+        self,
+        element: BeautifulSoup,
+        classes: list[str],
+    ) -> BeautifulSoup:
+        matching_elements = self._find_divs_with_classes(element, classes)
+        if len(matching_elements) != 1:
+            raise ValueError("Failed to locate element.")
+        return matching_elements[0]
